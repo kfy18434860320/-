43ad649149b3cff24f151bc67bc0de163aaa1a6c
kfy18434860320 <1227780476@qq.com>
2020-02-20 17:43:51 +0800

    商品项目

---
+++ app/src/main/java/com/example/shopping/ui/fenlei/FenleiFragment.java
@@ -0,0 +1,30 @@
+package com.example.shopping.ui.fenlei;
+
+
+import com.example.shopping.R;
+import com.example.shopping.base.BaseFragment;
+import com.example.shopping.interfaces.IBasePersenter;
+
+public class FenleiFragment extends BaseFragment {
+
+
+    @Override
+    protected int getLayout() {
+        return R.layout.fragment_fenlei;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected IBasePersenter createPersenter() {
+        return null;
+    }
+}
---
+++ app/src/main/java/com/example/shopping/ui/shopping/ShoppingFragment.java
@@ -0,0 +1,30 @@
+package com.example.shopping.ui.shopping;
+
+
+
+import com.example.shopping.R;
+import com.example.shopping.base.BaseFragment;
+import com.example.shopping.interfaces.IBasePersenter;
+
+
+public class ShoppingFragment extends BaseFragment {
+    @Override
+    protected int getLayout() {
+        return R.layout.fragment_shopping;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected IBasePersenter createPersenter() {
+        return null;
+    }
+}
