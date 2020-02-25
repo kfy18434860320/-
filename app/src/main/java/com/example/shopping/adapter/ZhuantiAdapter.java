8b873efd772b3fbd9df138a4d957e5fb3285f76a
kfy18434860320 <1227780476@qq.com>
2020-02-25 20:09:07 +0800

    专题页面

---
+++ app/src/main/java/com/example/shopping/adapter/ZhuantiAdapter.java
@@ -0,0 +1,36 @@
+package com.example.shopping.adapter;
+
+import android.content.Context;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.shopping.R;
+import com.example.shopping.base.BaseAdapter;
+import com.example.shopping.models.bean.ZhuantiBean;
+
+import java.util.List;
+
+public class ZhuantiAdapter extends BaseAdapter {
+
+
+    public ZhuantiAdapter(List data, Context context) {
+        super(data, context);
+    }
+
+    @Override
+    public int getLayout() {
+        return R.layout.item_zhuanti;
+    }
+
+    @Override
+    public void bindData(BaseViewHolder holder, Object o) {
+        ZhuantiBean.DataBeanX.DataBean dataBean = (ZhuantiBean.DataBeanX.DataBean) o;
+        ImageView img = (ImageView) holder.getView(R.id.img_rv_zhuanti);
+        TextView title = (TextView) holder.getView(R.id.title_zhaunti);
+        TextView subtitle = (TextView) holder.getView(R.id.subtitle);
+        Glide.with(mContext).load(dataBean.getScene_pic_url()).into(img);
+        title.setText(dataBean.getTitle());
+        subtitle.setText(dataBean.getSubtitle());
+    }
+}
--- app/src/main/java/com/example/shopping/ui/zhuanti/ZhuantiFragment.java
+++ app/src/main/java/com/example/shopping/ui/zhuanti/ZhuantiFragment.java
@@ -1,12 +1,29 @@
 package com.example.shopping.ui.zhuanti;
 
 
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.example.shopping.R;
+import com.example.shopping.adapter.ZhuantiAdapter;
 import com.example.shopping.base.BaseFragment;
-import com.example.shopping.interfaces.IBasePersenter;
+import com.example.shopping.interfaces.shangcheng.ZhuantiContract;
+import com.example.shopping.models.bean.ZhuantiBean;
+import com.example.shopping.persenter.zhuanti.ZhuantiPresenter;
 
-public class ZhuantiFragment extends BaseFragment {
+import java.util.ArrayList;
+import java.util.List;
 
+import butterknife.BindView;
+
+public class ZhuantiFragment extends BaseFragment<ZhuantiContract.Persenter> implements ZhuantiContract.View {
+
+
+    @BindView(R.id.rv_zhuanti)
+    RecyclerView rvZhuanti;
+    private ArrayList<ZhuantiBean.DataBeanX.DataBean> dataBeans;
+    private ZhuantiAdapter adapter;
 
     @Override
     protected int getLayout() {
@@ -15,16 +32,31 @@ public class ZhuantiFragment extends BaseFragment {
 
     @Override
     protected void initView() {
-
+        rvZhuanti.setLayoutManager(new LinearLayoutManager(context));
+        dataBeans = new ArrayList<>();
+        adapter = new ZhuantiAdapter(dataBeans,context);
+        rvZhuanti.setAdapter(adapter);
     }
+
 
     @Override
     protected void initData() {
-
+        int page = 1;
+        int size = 10;
+        persenter.getZhuantibean(page,size);
     }
 
     @Override
-    protected IBasePersenter createPersenter() {
-        return null;
+    protected ZhuantiContract.Persenter createPersenter() {
+        return new ZhuantiPresenter();
+    }
+
+
+
+    @Override
+    public void getZhuantiResult(ZhuantiBean result) {
+        List<ZhuantiBean.DataBeanX.DataBean> data = result.getData().getData();
+        dataBeans.addAll(data);
+        adapter.notifyDataSetChanged();
     }
 }
